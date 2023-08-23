af61f07593854bc52ff58b248ef9bc2226a6e745
AYFG <sek82468246@gmail.com>
2023-08-23 17:46:56 +0900

    prototype,constructor,class,privateProperty

--- 0822/009-foodRobot.js
+++ 0822/009-foodRobot.js
@@ -1,12 +1,21 @@
 // 실습 : 음식 이름의 배열을 전달하면 배열안에서 랜덤하게 메뉴를 뽑아내는 로봇객체의 생성자를 만들어보세요.
 
-function RobotFactory(foodNames) {
-  this.menu = foodNames;
-  this.sayMenu = function () {
-    // 배열의 길이에 해당하는 범위의 랜덤한 값을 뽑아낸다
-    const index = parseInt(Math.random() * foodNames.length);
-    console.log(`삐비비 오늘의 메뉴는 ${foodNames[index]}를 먹자`);
+// function RobotFactory(foodNames) {
+//   this.menu = foodNames;
+//   this.sayMenu = function () {
+//     // 배열의 길이에 해당하는 범위의 랜덤한 값을 뽑아낸다
+//     const index = parseInt(Math.random() * foodNames.length);
+//     console.log(`삐비비 오늘의 메뉴는 ${foodNames[index]}를 먹자`);
+//   };
+// }
+// const robot = new RobotFactory(["닭", "소", "돼지"]);
+// const robot2 = new RobotFactory(["오이", "호박", "당근"]);
+
+function ChatFoodAI(foodArr) {
+  this.menu = foodArr;
+  this.메뉴랜덤뽑기 = function () {
+    const randomIndex = parseInt(Math.random() * this.menu.length);
+    alert(`랜덤으로 뽑은 메뉴 ${this.menu[randomIndex]}`);
   };
 }
-const robot = new RobotFactory(["닭", "소", "돼지"]);
-const robot2 = new RobotFactory(["오이", "호박", "당근"]);
+const me = new ChatFoodAI(["짜장면", "짬뽕", "볶음밥", "탕수육"]);
--- 0822/010-prototype.js
+++ 0822/010-prototype.js
@@ -1,5 +1,11 @@
 // 프로토타입 : 모든 인스턴스들이 공유하는 공간
 // 손쉽게 객체를 생산할 수 있게 되었지만 객체의 메서드를 등록할 때마다 새로운 함수를 생성하고 있다. 자원의 낭비를 해결하기 위해 나온 것이 프로토타입
+this.sayYourName = function () {
+  console.log(`삐리비리. 제 이름은 ${this.name}입니다. 주인님.`);
+};
+const robot1 = new NewFactory(1);
+const robot2 = new NewFactory(2);
+
 function NewFactory2(name) {
   this.name = name;
 }
@@ -8,3 +14,9 @@ NewFactory2.prototype.sayYourName = function () {
   console.log(`삐리비리. 제 이름은 ${this.name}입니다. 주인님.`);
 };
 //이렇듯 프로토타입은 모든 인스턴스가 하나의 메서드를 공유하도록 만들어 자원을 더 효율적으로 사용하도록 도와줍니다. 생성된 객체끼리 메서드를 비교하여 같은 곳을 참조하는지 살펴보는것도 이해하는데 도움이 됩니다.
+
+//**프로토타입이란 무엇인가?**
+
+// prototype은 특정 객체에 대한 참조입니다. 즉 어떠한 공간을 가르키고 있습니다.
+
+// 생성자 함수가 인스턴스를 생성하게 되면 그 안에는 숨겨진 프로퍼티인 [[Prototype]] 이 존재하게 됩니다. 코드상에서는 `__proto__`로 표현됩니다. `__proto__` 프로퍼티는 자신을 만든 생성자 함수의 `prototype`을 참조하는 역할을 합니다. 즉, new 키워드를 통해 생성자 함수의 `prototype`과 인스턴스의 `__proto__` 가 연결됩니다.
