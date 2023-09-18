37aee0696452d4b6d92101bcced99c14411457e3
AYFG <sek82468246@gmail.com>
2023-09-18 16:55:05 +0900

    1만시간 html,css 먼저 만들고 react로 넘기는 작업

---
+++ 0918/react10000/src/App.js
@@ -0,0 +1,16 @@
+import Header from "./component/header/Header";
+import Main from "./component/main/Main";
+import Footer from "./component/footer/Footer";
+import Modal from "./component/modal/Modal";
+import "./index.css";
+function App() {
+  return (
+    <div id="app">
+      <Header />
+      <Main />
+      <Footer />
+      <Modal />
+    </div>
+  );
+}
+export default App;
