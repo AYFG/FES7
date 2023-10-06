3c50807a3e144cb44e37a4273f11b33d45ec0f5c
AYFG <sek82468246@gmail.com>
2023-10-06 13:37:47 +0900

    메인에 로그인 페이지 추가

---
+++ 1006/app/src/App.js
@@ -0,0 +1,12 @@
+import LoginPage from "./Components/LoginPage";
+
+function App() {
+  // 로그인 기능
+  return (
+    <div>
+      hello world
+      <LoginPage />
+    </div>
+  );
+}
+export default App;
